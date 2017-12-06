5b3ff3bec8e15a9f729cb81776f79f436f7fde3b
pankajladhar <ladharpankaj@gmail.com>
2017-12-06 17:40:33 +0530

    Created colorpicker component

---
+++ src/Components/ColorPicker/index.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { SketchPicker } from 'react-color';
+
+import './ColorPicker.css';
+
+class ColorPicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: this.props.color,
+            displayColorPicker: false
+        }
+        this.handOnClick = this.handOnClick.bind(this);
+        this.handOnChange = this.handOnChange.bind(this);
+        this.handOnClose = this.handOnClose.bind(this);
+    }
+
+    handOnClick() {
+        this.setState({ displayColorPicker: !this.state.displayColorPicker })
+    }
+
+    handOnChange(color) {
+        this.setState({ color: color.rgb },()=>{
+            if(this.props.isBackgroundColorPicker )
+            document.body.style.backgroundColor = `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`;
+        })
+        // this.props.handOnChange()
+    }
+
+    handOnClose() {
+        this.setState({ displayColorPicker: false })
+    }
+
+    __getStyleObject(isBackgroundColorPicker) {
+        const colorObject = {
+            color: `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`,
+        };
+
+        const backGroundObject = {
+            backgroundColor: `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`,
+        };
+
+        return isBackgroundColorPicker ? backGroundObject : colorObject
+
+    }
+
+    render() {
+        return (
+            <div className="ColorPicker">
+                <div className="ColorPicker__Swatch" onClick={this.handOnClick}>
+                    <div className="ColorPicker__Color" style={this.__getStyleObject(this.props.isBackgroundColorPicker)}>
+                        {!this.props.isBackgroundColorPicker && "A"}
+                    </div>
+                </div>
+                {this.state.displayColorPicker ?
+                    <div className="ColorPicker__Popover">
+                        <div className="ColorPicker__Cover" onClick={this.handOnClose} />
+                        <SketchPicker color={this.state.color} onChange={this.handOnChange} />
+                    </div> :
+                    null}
+
+            </div>
+        );
+    }
+}
+
+ColorPicker.propTypes = {
+
+};
+
+export default ColorPicker;
--- src/Components/FontOptionContainer/index.js
+++ src/Components/FontOptionContainer/index.js
@@ -3,9 +3,8 @@ import PropTypes from 'prop-types';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css'
 
-import reactCSS from 'reactcss';
-import { SketchPicker } from 'react-color';
 import WebFont from 'webfontloader';
+import ColorPicker from './../ColorPicker'
 import './FontOptionContainer.css'
 
 class FontOptionContainer extends Component {
@@ -27,22 +26,14 @@ class FontOptionContainer extends Component {
             },
             backGroundColor: {
                 r: '255',
-                g: '223',
-                b: '1',
+                g: '255',
+                b: '255',
                 a: '1',
             },
         }
 
         this.handleChangeFontFamily = this.handleChangeFontFamily.bind(this);
         this.handleChangeFontVariant = this.handleChangeFontVariant.bind(this);
-
-        this.handleClickOnForeGroundColorPicker = this.handleClickOnForeGroundColorPicker.bind(this);
-        this.handleCloseOnForeGroundColorPicker = this.handleCloseOnForeGroundColorPicker.bind(this);
-        this.handleChangeOnForeGroundColorPicker = this.handleChangeOnForeGroundColorPicker.bind(this);
-
-        this.handleClickOnBackGroundColorPicker = this.handleClickOnBackGroundColorPicker.bind(this);
-        this.handleCloseOnBackGroundColorPicker = this.handleCloseOnBackGroundColorPicker.bind(this);
-        this.handleChangeOnBackGroundColorPicker = this.handleChangeOnBackGroundColorPicker.bind(this);
     }
 
     handleChangeFontFamily = (selectedFontFamily) => {
@@ -61,43 +52,7 @@ class FontOptionContainer extends Component {
         this.props.handleChangeFontVariant(selectedFontVariant)
     }
 
-    handleClickOnForeGroundColorPicker = () => {
-        this.setState({ displayForeGroundColorPicker: !this.state.displayForeGroundColorPicker })
-    };
-
-    handleCloseOnForeGroundColorPicker = () => {
-        this.setState({ displayForeGroundColorPicker: false })
-    };
-
-    handleChangeOnForeGroundColorPicker = (color) => {
-        this.setState({ foreGroundColor: color.rgb })
-    };
-
-    handleClickOnBackGroundColorPicker = () => {
-        this.setState({ displayBackGroundColorPicker: !this.state.displayBackGroundColorPicker })
-    };
-
-    handleCloseOnBackGroundColorPicker = () => {
-        this.setState({ displayBackGroundColorPicker: false })
-    };
-
-    handleChangeOnBackGroundColorPicker = (color) => {
-        this.setState({ backGroundColor: color.rgb },()=>{
-            document.body.style.backgroundColor = `rgba(${this.state.backGroundColor.r}, ${this.state.backGroundColor.g}, ${this.state.backGroundColor.b}, ${this.state.backGroundColor.a})`;
-        })
-    };
-
     render() {
-        const styles = reactCSS({
-            'default': {
-                foreGroundColor: {
-                    color: `rgba(${this.state.foreGroundColor.r}, ${this.state.foreGroundColor.g}, ${this.state.foreGroundColor.b}, ${this.state.backGroundColor.a})`,
-                },
-                backGroundColor: {
-                    background: `rgba(${this.state.backGroundColor.r}, ${this.state.backGroundColor.g}, ${this.state.backGroundColor.b}, ${this.state.backGroundColor.a})`,
-                }
-            },
-        });
         return (
             <section className="FontOptionContainer">
                 <section className="FontOptionContainer__Item">
@@ -138,31 +93,12 @@ class FontOptionContainer extends Component {
                                 <input type="number" name="quantity" min="6" max="248" value="16" />
                             </div>
                             <div className="ColorOption__ForeGround">
-                                <div className="ColorOption--Swatch" onClick={this.handleClickOnForeGroundColorPicker}>
-                                    <div className="ColorOption--Color" style={styles.foreGroundColor}>
-                                        A
-                                    </div>
-                                </div>
-                                {this.state.displayForeGroundColorPicker ?
-                                    <div className="ColorOption--Popover">
-                                        <div className="ColorOption--Cover" onClick={this.handleCloseOnForeGroundColorPicker} />
-                                        <SketchPicker color={this.state.foreGroundColor} onChange={this.handleChangeOnForeGroundColorPicker} />
-                                    </div> :
-                                    null}
-
+                                <ColorPicker color={this.state.foreGroundColor}/>
                             </div>
-
                             <div className="ColorOption__BackGround">
-                                <div className="ColorOption--Swatch" onClick={this.handleClickOnBackGroundColorPicker}>
-                                    <div className="ColorOption--Color" style={styles.backGroundColor} />
-                                </div>
-                                {this.state.displayBackGroundColorPicker ?
-                                    <div className="ColorOption--Popover">
-                                        <div className="ColorOption--Cover" onClick={this.handleCloseOnBackGroundColorPicker} />
-                                        <SketchPicker color={this.state.backGroundColor} onChange={this.handleChangeOnBackGroundColorPicker} />
-                                    </div> :
-                                    null}
-
+                                <ColorPicker color={this.state.backGroundColor}
+                                    isBackgroundColorPicker
+                                />
                             </div>
                         </div>
 
