629091cd1fbfb62737ba6cf15e295a0f639d3b6b
pankajladhar <ladharpankaj@gmail.com>
2017-12-11 14:13:28 +0530

    Fixing build script

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ mv ./build/index.html index.html
 mv ./build/favicon.ico favicon.ico
 mv ./build/asset-manifest.json asset-manifest.json
 mv ./build/service-worker.js service-worker.js
-mb ./build/manifest.json manifest.json
-mb ./build/font-awesome.css font-awesome.css
+mv ./build/manifest.json manifest.json
+mv ./build/font-awesome.css font-awesome.css
 mv ./build/static/ static
 mv ./build/fonts/ fonts
 
