14f4fa5cfcded10a43ee5b30f06510e7c0a57129
pankajladhar <ladharpankaj@gmail.com>
2017-12-11 14:03:08 +0530

    Fixing build.sh

--- build.sh
+++ build.sh
@@ -9,8 +9,10 @@ mv ./build/index.html index.html
 mv ./build/favicon.ico favicon.ico
 mv ./build/asset-manifest.json asset-manifest.json
 mv ./build/service-worker.js service-worker.js
-mb ./buildmanifest.json manifest.json
+mb ./build/manifest.json manifest.json
+mb ./build/font-awesome.css font-awesome.css
 mv ./build/static/ static
+mv ./build/fonts/ fonts
 
 today=`date '+%Y_%m_%d__%H_%M_%S'`;
 git add .
